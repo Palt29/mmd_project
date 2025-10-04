4ff355f6c8d79bf02e34a35a7c9bb03df1224821
Pedro Lopez <lotopedro29@gmail.com>
2025-10-04 23:54:58 +0200

    Minor changes

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 def main() -> None:
     import logging
 
+    import numpy as np
+
+    from src.loocv import loocv_split
+
     # Logger setup
     logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
     logging.getLogger(__name__)
+
+    # rGLSVD algorithm pipeline
+    R_bin = np.load("utility_matrix.npy")
+    clusters = np.load("clusters.npy")
 
     num_clusters = 7
     # Global rank for SVD
@@ -21,7 +29,10 @@ def main() -> None:
         },
     )  # test numbers
 
-    print(num_clusters, f_g, f_c_dict)
+    # 1. LOOCV SPLIT
+    train_matrix, test_items = loocv_split(R_bin)
+
+    print(clusters, num_clusters, f_g, f_c_dict, train_matrix, test_items)
 
 
 if __name__ == "__main__":
