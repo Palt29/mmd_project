2924127264825eb36ab190088b16b4eee1ace348
Pedro Lopez <lotopedro29@gmail.com>
2025-10-04 23:15:32 +0200

    Add variables for rGLSVD algorithm to main

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 def main() -> None:
-    print("Hello from mmd-project!")
+    import logging
+
+    # Logger setup
+    logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
+    logging.getLogger(__name__)
+
+    num_clusters = 7
+    # Global rank for SVD
+    f_g = 18
+    # Local ranks (one for each cluster) for local SVD
+    f_c_dict = (
+        {
+            0: 7,
+            1: 5,
+            2: 10,
+            3: 8,
+            4: 6,
+            5: 7,
+            6: 6,
+        },
+    )  # test numbers
+
+    print(num_clusters, f_g, f_c_dict)
 
 
 if __name__ == "__main__":
